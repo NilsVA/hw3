4cb5b9122fe0b706b1824907455a384d3c61783b
NilsVA <you@example.com>
2015-11-17 18:57:19 +0100

    hw3

--- statistics.cxx
+++ statistics.cxx
@@ -1,19 +1,41 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 //--------------------------------------------------
 // plenty of lines here
 //--------------------------------------------------
 
+void randomnr(double* p , const int N)
+	{
+	 for(int i=0; i<N; i++)
+	     p[i]=(double)rand() / RAND_MAX;
+	}
+
+void statistics(double* p, const int N, double& mean, double& var)
+	{
+	 double sum = 0.0;
+	 double sumvar = 0.0;
+	 	for(int i=0; i<N; i++)
+			 sum += p[i];
+	 mean = sum/N;
+	 	for(int i=0; i<N; i++)
+			 sumvar += pow((p[i]-mean),2);
+	 var = sumvar/N;	
+	}
+
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
-
+   srand(time(NULL));
    // Some lines here....
-
+   randomnr(p,N);
+   for(int j=0; j<N;j++)
+	cout << p[j] << endl;
+   statistics(p,N,mean,var);
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
